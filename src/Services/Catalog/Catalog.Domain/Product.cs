a374657c8688092d1c0ff29405c23de976264e70
Nelson Ortiz Leal <NelsonOrtizLeal@gmail.com>
2024-11-07 17:30:48 -0600

    Add: Agregando biblioteca de clase de tipo EL/Dominio de catalogo

---
+++ src/Services/Catalog/Catalog.Domain/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace Catalog.Domain
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public ProductInStock Stock { get; set; }
+
+        // Para crear una relacion de 1 - 1 con ProductInStock
+        // public ProductInStock Stock { get; set; }
+
+        // Para crear una relacion de 1 - muchos con ProductInStock
+        // public List<ProductInStock> Stock { get; set; }
+    }
+}
