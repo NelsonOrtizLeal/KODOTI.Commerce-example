a40699e61229ac73cd1c80bcd7e89c501b5cc586
Nelson Ortiz Leal <NelsonOrtizLeal@gmail.com>
2024-11-12 16:31:37 -0600

    Update: creando parametros de conexion y DbSet

--- src/Services/Catalog/Catalog.Persistence.Database/ApplicationDbContext.cs
+++ src/Services/Catalog/Catalog.Persistence.Database/ApplicationDbContext.cs
@@ -1,11 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Catalog.Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace Catalog.Persistence.Database
 {
     public class ApplicationDbContext : DbContext
     {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductInStock> Stocks { get; set; }
     }
 }
