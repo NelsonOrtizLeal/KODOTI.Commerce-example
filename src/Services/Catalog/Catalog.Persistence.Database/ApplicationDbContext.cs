319879bd78c160fb7a8a14e616dfe961bf7d1bfd
Nelson Ortiz Leal <NelsonOrtizLeal@gmail.com>
2024-11-12 16:27:26 -0600

    Add: agregando clase que tendra el contexto de conexión a la BD

---
+++ src/Services/Catalog/Catalog.Persistence.Database/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Persistence.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+    }
+}
